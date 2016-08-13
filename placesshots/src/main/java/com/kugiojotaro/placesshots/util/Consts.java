173d307abaca858c1729afd93a5d6be23d988d15
OraOraOrraah <oraoraorraah@gmail.com>
2016-08-13 12:57:22 +0700

    20160813

---
+++ placesshots/src/main/java/com/kugiojotaro/placesshots/util/Consts.java
@@ -0,0 +1,30 @@
+package com.kugiojotaro.placesshots.util;
+
+public class Consts {
+	
+	public static final String AUTHEN_USER= "authUser";
+	
+	public static final String MODE_ADD = "add";
+	public static final String MODE_EDIT = "edit";
+	public static final String RESULT_SUCCESS = "success";
+	public static final String RESULT_FAIL = "fail";
+	
+	public static final short EURO_2016 = 1;
+	
+	public static final short BEGIN_LIVE_HOUR = 14;
+	public static final short END_LIVE_HOUR = 5;
+	
+	public static final String LIVE = "live";
+	
+	public static final String WEEK = "week";
+	public static final String ROUND = "round";
+	
+	public static final String ROUND_11 = "11";
+	public static final String ROUND_12 = "12";
+	public static final String ROUND_13 = "13";
+	public static final String ROUND_16 = "16";
+	public static final String ROUND_8 = "8";
+	public static final String ROUND_4 = "4";
+	public static final String ROUND_2 = "2";
+
+}
