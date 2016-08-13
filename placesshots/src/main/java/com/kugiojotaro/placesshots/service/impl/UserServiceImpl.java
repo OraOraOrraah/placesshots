8d4227446e5d204ea96360ca0602e3111b83672d
OraOraOrraah <oraoraorraah@gmail.com>
2016-08-13 13:28:28 +0700

    20160813

--- placesshots/src/main/java/com/kugiojotaro/placesshots/service/impl/UserServiceImpl.java
+++ placesshots/src/main/java/com/kugiojotaro/placesshots/service/impl/UserServiceImpl.java
@@ -2,27 +2,27 @@ package com.kugiojotaro.placesshots.service.impl;
 
 import java.util.Date;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.kugiojotaro.placesshots.dao.UserDao;
 import com.kugiojotaro.placesshots.dto.UserChangePasswordDto;
 import com.kugiojotaro.placesshots.dto.UserDto;
 import com.kugiojotaro.placesshots.entity.User;
 import com.kugiojotaro.placesshots.mapper.UserMapper;
+import com.kugiojotaro.placesshots.repository.UserRepository;
 import com.kugiojotaro.placesshots.service.UserService;
 
+import lombok.extern.log4j.Log4j;
+
 @Service
+@Log4j
 public class UserServiceImpl implements UserService {
 
-	private static final Logger LOGGER = Logger.getLogger(UserServiceImpl.class);
-
 	@Autowired
-	private UserDao userDao;
+	private UserRepository userRepository;
 	
 	@Autowired
 	private UserMapper userMapper;
@@ -32,18 +32,18 @@ public class UserServiceImpl implements UserService {
 	
 	@Override
 	public Boolean create(UserDto userDto) {
-		LOGGER.debug(" create user: " + userDto.getUsername());
+		log.debug(" create user: " + userDto.getUsername());
 		
 		try {
 			User user = userMapper.toPersistenceBean(userDto);
 			user.setPassword(md5PasswordEncoder.encodePassword(userDto.getPassword(), null));
 			user.setCreateDate(new Date());
-			userDao.save(user);
+			userRepository.save(user);
 			
-			LOGGER.info(" registered user: " + userDto.getUsername());
+			log.info(" registered user: " + userDto.getUsername());
 		}
 		catch (Exception ex) {
-			LOGGER.error(ex, ex);
+			log.error(ex, ex);
 			return false;
 		}
 		
@@ -52,17 +52,17 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public Boolean update(UserDto userDto) {
-		LOGGER.debug(" update");
+		log.debug(" update");
 		
 		try {
-			User user = userDao.findByUsername(userDto.getUsername());
+			User user = userRepository.findByUsername(userDto.getUsername());
 			user.setPassword(md5PasswordEncoder.encodePassword(userDto.getPassword(), null));
 			user = userMapper.toPersistenceBean(userDto);
 			user.setUpdateDate(new Date());
-			userDao.save(user);
+			userRepository.save(user);
 		}
 		catch (Exception ex) {
-			LOGGER.error(ex, ex);
+			log.error(ex, ex);
 			return false;
 		}
 		
@@ -71,15 +71,15 @@ public class UserServiceImpl implements UserService {
 	
 	@Override
 	public Boolean updateProfile(UserDto userDto) {
-		LOGGER.debug(" update");
+		log.debug(" update");
 		
 		try {
-			User user = userDao.findByUsername(userDto.getUsername());
+			User user = userRepository.findByUsername(userDto.getUsername());
 			user.setUpdateDate(new Date());
-			userDao.save(user);
+			userRepository.save(user);
 		}
 		catch (Exception ex) {
-			LOGGER.error(ex, ex);
+			log.error(ex, ex);
 			return false;
 		}
 		
@@ -88,19 +88,19 @@ public class UserServiceImpl implements UserService {
 	
 	@Override
 	public Boolean delete(String username) {
-		LOGGER.debug(" delete");
+		log.debug(" delete");
 		
 		return true;
 	}
 
 	@Override
 	public UserDto findByUsername(String username) {
-		LOGGER.debug(" findByUsername (username: " + username + ")");
+		log.debug(" findByUsername (username: " + username + ")");
 		
 		UserDto userDto = null;
 		
 		try {
-			User user = userDao.findByUsername(username);
+			User user = userRepository.findByUsername(username);
 			if (user != null) {
 				userDto = userMapper.toDtoBean(user);
 				if (user.getUserConnection() == null) {
@@ -113,7 +113,7 @@ public class UserServiceImpl implements UserService {
 			}
 		}
 		catch (Exception ex) {
-			LOGGER.error(ex, ex);
+			log.error(ex, ex);
 		}
 		
 		return userDto;
@@ -121,12 +121,12 @@ public class UserServiceImpl implements UserService {
 	
 	@Override
 	public UserDto findByUsernameAndPassword(String username, String password) {
-		LOGGER.debug(" findByUsernameAndPassword, username: " + username);
+		log.debug(" findByUsernameAndPassword, username: " + username);
 		
 		UserDto userDto = null;
 		
 		try {
-			User user = userDao.findByUsername(username);
+			User user = userRepository.findByUsername(username);
 			if (user != null) {
 				String md5Password = md5PasswordEncoder.encodePassword(password, null);
 				if (user.getPassword().equals(md5Password)) {
@@ -135,7 +135,7 @@ public class UserServiceImpl implements UserService {
 			}
 		}
 		catch (Exception ex) {
-			LOGGER.error(ex, ex);
+			log.error(ex, ex);
 		}
 		
 		return userDto;
@@ -148,16 +148,16 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public Boolean changepassword(UserChangePasswordDto userChangePasswordDto) {
-		LOGGER.info(" changepassword (username: " + userChangePasswordDto.getUsername() + ")");
+		log.info(" changepassword (username: " + userChangePasswordDto.getUsername() + ")");
 		try {
-			User user = userDao.findByUsername(userChangePasswordDto.getUsername());
+			User user = userRepository.findByUsername(userChangePasswordDto.getUsername());
 			user.setPassword(md5PasswordEncoder.encodePassword(userChangePasswordDto.getPassword(), null));
-			userDao.save(user);
-			LOGGER.info(" changepassword success");
+			userRepository.save(user);
+			log.info(" changepassword success");
 			return true;
 		}
 		catch (Exception ex) {
-			LOGGER.error(ex, ex);
+			log.error(ex, ex);
 			return false;
 		}
 	}
